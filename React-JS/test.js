b21bc44d143fad18c12ef2ef86d5f6c00ab5aa32
Viktar Taustyka <taustyka.viktar@gmail.com>
2023-11-07 18:04:06 +0100

    several examples - C3

--- React-JS/test.js
+++ React-JS/test.js
@@ -1,1 +1,5 @@
 console.log("Hello")
+// Path: React-JS/test.js
+// q: What does SOLID stand for?
+// a: Single Responsibility Principle, Open-Closed Principle, 
+//    Liskov Substitution Principle, Interface Segregation Principle, Dependency Inversion Principle
